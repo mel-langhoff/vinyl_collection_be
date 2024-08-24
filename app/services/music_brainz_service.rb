aea521e9c916879c5e72974e5a3f582b01631a7a
Mel <mel@Mels-MacBook-Pro.local>
2024-08-24 17:03:27 -0600

    Spec file passes, #fetch_song_data completed

---
+++ app/services/music_brainz_service.rb
@@ -0,0 +1,19 @@
+class MusicBrainzService
+  def self.fetch_song_data(mbid)
+    get_url("recording/#{mbid}")
+  end
+
+  private 
+
+  def self.conn
+    Faraday.new(url: 'https://musicbrainz.org/ws/2/') do |faraday|
+      faraday.headers['User-Agent'] = 'MelLanghoffVinylsAPI/1.0 (hello@mel-langhoff.com)'
+      faraday.params[:fmt] = 'json'
+    end
+  end
+
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
